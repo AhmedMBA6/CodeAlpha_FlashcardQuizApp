d57710e557b4ea816db9f0177af07ad6c127b97a
AhmedMBA6 <ahmed.bauiomy17@gmail.com>
2025-06-20 21:37:44 +0300

    - Edit project setup with clean architecture - setting development  and production flavors for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main({String flavor = 'prod'}) {
+  runApp(MyApp(flavor: flavor));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String flavor;
+  const MyApp({super.key, required this.flavor});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(appBar: AppBar(title: Text('Hello World')), body: Center(child: Text('Hello World'),),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello World ($flavor)')),
+        body: Center(child: Text('Hello World - $flavor')),
+      ),
+    );
   }
 }
